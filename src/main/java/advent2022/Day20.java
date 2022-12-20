83c01731ff5d82597f00e4ff33490cb55a908969
Guillaume Dufrêne <guillaume.dufrene@axa.fr>
2022-12-20 11:43:48 +0100

    Clean, optimize and prepare part 2

--- src/main/java/advent2022/Day20.java
+++ src/main/java/advent2022/Day20.java
@@ -30,10 +30,29 @@ public class Day20 implements AdventDay {
 
     public void part1() throws IOException {
         ArrayList<Integer> indexes = new ArrayList<>(4000);
-        int size = lines.size();  
+        int size = lines.size();
         for (int i = 0; i < size; i++) {
             indexes.add(i);
         }
+        
+        mix( indexes );
+        
+        int zero = lines.indexOf(0);
+        int sum = 0;
+        sum += lines.get( (zero + 1000) % size );
+        sum += lines.get( (zero + 2000) % size );
+        sum += lines.get( (zero + 3000) % size );
+        
+        // -5520 xxx
+        // -8370 xxx
+        // -5447 xxx
+        // 988 IS OK !!
+        System.out.println( sum );
+        
+    }
+    
+    private void mix(ArrayList<Integer> indexes) {
+        int size = indexes.size();
         int delta = 0;
         int size2 = size-1; // when remove one element ...
         for (int i = 0; i < size; i++) {
@@ -48,59 +67,19 @@ public class Day20 implements AdventDay {
             int nidx = idelta;
             
             if ( val < 0 ) {
-                for (int j = 0; j < Math.abs(val); j++) {
-                    nidx--;
-                    if (nidx<0) nidx += size2;
-                }
+                nidx += val;
+                while ( nidx < 0 ) nidx += size2;
                 if ( nidx == 0 ) nidx = size2;
             } else {
-                for (int j = 0; j < Math.abs(val); j++) {
-                    nidx++;
-                    if (nidx>size2) nidx -= size2;
-                }
+                nidx = (nidx+val)%size2;
                 if (nidx == size2) nidx = 0;
             }
             
             lines.add(nidx, val);
             indexes.add(nidx, i);
-            
-            
-            /*
-            if ( val == 0 ) continue; // zero !
-            int nidx = idelta + val;
-            if ( nidx == size - 1 ) nidx = 0 ;
-            else if ( nidx >= size ) {
-                nidx = nidx % size;
-                nidx++;
-            }
-            if ( nidx < 0 ) {
-                // nidx--;
-                while ( nidx < 0 ) nidx += size;
-            }
-            if ( nidx < idelta ) {
-                if ( nidx == 0 ) nidx = size - 1;
-            }
-            lines.remove(idelta);
-            indexes.remove(idelta);
-            lines.add(nidx, val);
-            indexes.add(nidx, i);
-            */
 //            System.out.format( "%d moves at %d\n", val, nidx);
 //            show();
         }
-        
-        int zero = lines.indexOf(0);
-        int sum = 0;
-        sum += lines.get( (zero + 1000) % size );
-        sum += lines.get( (zero + 2000) % size );
-        sum += lines.get( (zero + 3000) % size );
-        
-        // -5520 xxx
-        // -8370 xxx
-        // -5447 xxx
-        // 988 IS OK !!
-        System.out.println( sum );
-        
     }
     
     private void show() {
