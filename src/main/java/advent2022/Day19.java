dc6eeb1ecdfb7a84d26f5471fb2b8842b8a2c174
Guillaume Dufrêne <guillaume.dufrene@axa.fr>
2022-12-20 11:16:21 +0100

    Day 20, 2 tries ... failed

--- src/main/java/advent2022/Day19.java
+++ src/main/java/advent2022/Day19.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.regex.Matcher;
@@ -19,7 +20,7 @@ public class Day19 implements AdventDay {
     
     public static void main(String[] args) throws Exception {
         new Day19()
-            .load( Filename.SAMPLE.filename() )
+            .load( Filename.DATA.filename() )
             .part1();
     }
     
@@ -50,48 +51,167 @@ public class Day19 implements AdventDay {
 
     public void part1() throws IOException {
         
+        //int cost[] = new int[] { 2, 4, 4, 15, 2, 20 };
+        int i = 0;
+        int sum = 0;
+        for( Blueprint bp : blueprints ) {
+            i++;
+            int geode = maxGeode(bp);
+            int qualityLevel = i * geode;
+            sum += qualityLevel;
+            System.out.format( "%d x %d = %d\n", i, geode, qualityLevel );
+        }
+        System.out.println( "PART1 ===> " + sum );
         
-        int maxTime = 24;
+        // 2193
         
-        Blueprint bp = blueprints.get(0);
-        LinkedList<Blueprint> backChoices = new LinkedList<>();
-        backChoices.add(bp);
-        
-        while( !backChoices.isEmpty() ) {
-            bp = backChoices.removeFirst();
-            while(bp.time < maxTime) {
-                bp.buyOptions()
-                    .stream()
-                    .map( opt -> { opt.produce(); return opt;} )
-                    .forEach( backChoices::add );
-                bp.produce();
+    }
+    
+    int maxGeode(Blueprint bp) {
+        Blueprint maxBp = null;
+        int maxValue = 0;
+        int[] maxBuys = null;
+        for (int i = 25; i >= 0; i--) {
+            for (int j = 25; j >= 0; j--) {
+                for (int k = 25; k >= 0; k--) {                    
+                    int buys[] = new int[] {i, j, k, 25}; 
+                    Blueprint bp1 = bp.copy();
+                    geode(bp1, buys);
+                    // System.out.println( Arrays.toString(bp1.stock) );
+                    if ( bp1.stock[OBSIDIAN] >= maxValue ) {
+                        maxBp = bp1;
+                        maxValue = bp1.stock[OBSIDIAN];
+                        maxBuys = buys;
+                    }
+                }
             }
-            System.out.println("BackChoice size : " + backChoices.size());
-            System.out.println( bp.stockString() );
         }
         
+        // int[] maxBuys = null;
+        maxValue = 0;
+        for (int i = 25; i >= 0; i--) {
+            for (int j = 25; j >= 0; j--) {
+                int buys[] = new int[] {maxBuys[0], maxBuys[1], maxBuys[2], j}; 
+                Blueprint bp1 = bp.copy();
+                geode(bp1, buys);
+                // System.out.println( Arrays.toString(bp1.stock) );
+                if ( bp1.stock[GEODE] >= maxValue ) {
+                    maxBp = bp1;
+                    maxValue = bp1.stock[GEODE];
+                    maxBuys = buys;
+                }
+            }
+        }
+        
+//        System.out.println("-- To Geode --");
+//        System.out.println( Arrays.toString(maxBp.stock) );
+        // System.out.println( Arrays.toString(maxBuys) );
+        return maxBp.stock[GEODE];
+    }
+    
+    int geode(Blueprint bp, int buyAt[]) {
+        
+        int cost[]  = bp.costs; //new int[] { 4, 2, 3, 14, 2, 7 };
+        int stock[] = bp.stock; // int[] { 0, 0, 0, 0 }; 
+        int prod[]  = bp.prod; //  int[] { 1, 0, 0, 0 }; 
+        
+        boolean needOre = false;
+        for (int i = 0; i < 24; i++) {
+            
+            int buy = -1;
+            
+            int buyOreAt = buyAt[ORE];
+            int buyClayAt = buyAt[CLAY];
+            int buyObsAt = buyAt[OBSIDIAN];
+            int buyGeodeAt = buyAt[GEODE];
+            
+            if ( i >= buyGeodeAt && stock[OBSIDIAN] >= cost[5] ) {
+                if ( stock[ORE] >= cost[4] ) buy = GEODE;
+                // else needOre = true;
+            }
+            else if ( i >= buyObsAt && stock[CLAY] >= cost[3]) {
+                if ( stock[ORE] >= cost[2] ) buy = OBSIDIAN;
+                // else needOre = true;
+            }
+            else if ( !needOre && i >= buyClayAt && stock[ORE] >= cost[1] ) buy = CLAY;
+            else if ( !needOre && i >= buyOreAt && stock[ORE] >= cost[0] ) buy = ORE;
+            
+            if ( buy >= 0 ) { 
+                if ( buy == ORE ) { stock[ORE] -= cost[0]; }
+                else if ( buy == CLAY ) { stock[ORE] -= cost[1]; }
+                else if ( buy == OBSIDIAN ) { stock[ORE] -= cost[2]; stock[CLAY] -= cost[3]; }
+                else if ( buy == GEODE ) { stock[ORE] -= cost[4]; stock[OBSIDIAN] -= cost[5]; }
+            }
+            
+            for (int j = 0; j < prod.length; j++) {
+                stock[j] += prod[j];
+            }
+            
+            if (buy >= 0) prod[buy]++;
+        }
+        
+        /*
+        System.out.format("[%02d]: Ore:%3d Clay:%3d Obs:%3d Geo:%3d\n", 24, 
+                stock[ORE], stock[CLAY], stock[OBSIDIAN], stock[GEODE] );
+                */
+        
+        return stock[GEODE];
     }
     
     public void part2() throws IOException {
-        
+        // 7200
     }
     
     
-    public static int
+    public final static int
+        NONE     = -1,
         ORE      = 0,
         CLAY     = 1,
         OBSIDIAN = 2,
         GEODE    = 3;
     
+    public static class Task {
+        int[] needs;
+        int atTime;
+        int toBuild;
+        public Task(int toBuild) {
+            this.toBuild = toBuild;
+        }
+    }
+    
     public static class Blueprint {
         int[] costs;
-        int[] robots = new int[4];
+        int[] prod  = new int[4];
         int[] stock = new int[4];
         int buy = -1;
         int time;
         public Blueprint(int[] costs) {
             this.costs = costs;
-            robots[ORE] = 1;
+            prod[ORE] = 1;
+        }
+        public void apply(Task t) {
+            // TODO Auto-generated method stub
+            
+        }
+        public Optional<Task> requirement(Task t) {
+            int requirement = switch (t.toBuild) {
+                case ORE -> stock[ORE] >= costs[0] ? NONE : ORE;
+                case CLAY -> stock[ORE] >= costs[1] ? NONE : ORE;
+                case OBSIDIAN -> {
+                    if (stock[ORE] < costs[2]) yield ORE;
+                    if (stock[CLAY] < costs[3]) yield CLAY;
+                    yield NONE;
+                }
+                case GEODE -> { 
+                    if (stock[ORE] < costs[4]) yield ORE;
+                    if (stock[OBSIDIAN] < costs[5]) yield OBSIDIAN;
+                    yield NONE;
+                }
+                default -> NONE;
+            };
+            if ( requirement == NONE ) return Optional.empty();
+            
+            return null;
         }
         public String stockString() {
             return String.format("ORE[%2d] CLAY[%2d] OBSID[%2d] GEODE[%2d]", stock[ORE], stock[CLAY], stock[OBSIDIAN], stock[GEODE]);
@@ -139,14 +259,14 @@ public class Day19 implements AdventDay {
             Blueprint p = new Blueprint(costs);
             p.time = time;
             p.stock = Arrays.copyOf(stock, 4);
-            p.robots = Arrays.copyOf(robots, 4);
+            p.prod = Arrays.copyOf(prod, 4);
             return p;
         }
         void produce() {
-            for (int i = 0; i < robots.length; i++)
-                stock[i] += robots[i];
+            for (int i = 0; i < prod.length; i++)
+                stock[i] += prod[i];
             if ( buy >= 0 ) {
-                robots[buy]++;
+                prod[buy]++;
                 buy = 0;
             }
             time++;
---
+++ src/main/java/advent2022/Day20.java
@@ -0,0 +1,119 @@
+package advent2022;
+
+import static java.lang.Integer.parseInt;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Day20 implements AdventDay {
+    
+    
+    public static void main(String[] args) throws Exception {
+        new Day20()
+            .load( Filename.DATA.filename() )
+            .part1();
+    }
+    
+    ArrayList<Integer> lines = new ArrayList<>(4000);
+    
+    
+    private Day20 load(String f) throws IOException {
+        Files.lines( dayFile(f) )
+                .map(Integer::parseInt)
+                .forEach( lines::add );
+        return this;
+    }
+
+    public void part1() throws IOException {
+        int size = lines.size();
+        ArrayList<Integer> indexes = new ArrayList<>(4000);
+        for (int i = 0; i < size; i++) {
+            indexes.add(i);
+        }
+        int delta = 0;
+        for (int i = 0; i < size; i++) {
+            int idelta = (i + delta) % size;
+            while ( indexes.get(idelta) != i ) {
+                delta = (delta + 1) % size;
+                idelta = (i + delta) % size;
+            }
+            int val = lines.get(idelta);
+            int nidx = idelta;
+            
+            if ( val < 0 ) {
+                for (int j = 0; j < Math.abs(val); j++) {
+                    nidx--;
+                    if (nidx<0) nidx += size;
+                }
+                if ( nidx == 0 ) nidx = size;
+            } else {
+                for (int j = 0; j <= Math.abs(val); j++) {
+                    nidx++;
+                    if (nidx>size) nidx -= size;
+                }
+                if (nidx == size) nidx = 0;
+            }
+            
+            lines.add(nidx, val);
+            indexes.add(nidx, i);
+            if (nidx < idelta) idelta++;
+            lines.remove(idelta);
+            indexes.remove(idelta);
+            
+            
+            /*
+            if ( val == 0 ) continue; // zero !
+            int nidx = idelta + val;
+            if ( nidx == size - 1 ) nidx = 0 ;
+            else if ( nidx >= size ) {
+                nidx = nidx % size;
+                nidx++;
+            }
+            if ( nidx < 0 ) {
+                // nidx--;
+                while ( nidx < 0 ) nidx += size;
+            }
+            if ( nidx < idelta ) {
+                if ( nidx == 0 ) nidx = size - 1;
+            }
+            lines.remove(idelta);
+            indexes.remove(idelta);
+            lines.add(nidx, val);
+            indexes.add(nidx, i);
+            */
+//            System.out.format( "%d moves at %d\n", val, nidx);
+//            show();
+        }
+        
+        int zero = lines.indexOf(0);
+        int sum = 0;
+        sum += lines.get( (zero + 1000) % size );
+        sum += lines.get( (zero + 2000) % size );
+        sum += lines.get( (zero + 3000) % size );
+        
+        // -5520 xxx
+        // -8370 xxx
+        
+        System.out.println( sum );
+        
+    }
+    
+    private void show() {
+        lines.forEach( v -> System.out.print(v + " ") );
+        System.out.println();
+    }
+    
+    public void part2() throws IOException {
+        
+    }
+    
+    
+
+    
+    
+}
+
